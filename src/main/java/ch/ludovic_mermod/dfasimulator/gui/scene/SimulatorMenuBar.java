b1bd885905fe78589a46cb6e9fa2fa4166e44953
Ludovic Mermod <ludovic.mermod@epfl.ch>
2022-03-04 21:39:45 +0100

    Reformat

--- src/main/java/ch/ludovic_mermod/dfasimulator/gui/scene/SimulatorMenuBar.java
+++ src/main/java/ch/ludovic_mermod/dfasimulator/gui/scene/SimulatorMenuBar.java
@@ -7,7 +7,6 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
