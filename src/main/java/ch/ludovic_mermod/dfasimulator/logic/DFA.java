d8cd85fa4a18a04d859ca1275957af7139bb5f21
Ludovic Mermod <ludovic.mermod@epfl.ch>
2022-03-12 23:05:22 +0100

    Change DFA into a record

--- src/main/java/ch/ludovic_mermod/dfasimulator/logic/DFA.java
+++ src/main/java/ch/ludovic_mermod/dfasimulator/logic/DFA.java
@@ -3,15 +3,11 @@ package ch.ludovic_mermod.dfasimulator.logic;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.concurrent.atomic.AtomicReference;
 
-public class DFA
+public record DFA(String initialState, Set<String> states,
+                  Set<String> acceptingStates, Set<Character> alphabet,
+                  Map<String, Map<Character, String>> transitionMap)
 {
-    private final String initialState;
-    private final Set<String> states, acceptingStates;
-    private final Set<Character> alphabet;
-    private final Map<String, Map<Character, String>> transitionMap;
-
     public DFA(String initialState, Set<String> states, Set<String> acceptingStates, Set<Character> alphabet, Map<String, Map<Character, String>> transitionMap)
     {
         this.initialState = initialState;
@@ -24,27 +20,6 @@ public class DFA
     private static Map<String, Map<Character, String>> copyTransitionMap(Map<String, Map<Character, String>> transitionMap)
     {
         return Map.copyOf(transitionMap.entrySet().stream().map(e -> Map.entry(e.getKey(), Map.copyOf(e.getValue()))).collect(TreeMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), TreeMap::putAll));
-    }
-
-    public String getInitialState()
-    {
-        return initialState;
-    }
-    public Set<String> getStates()
-    {
-        return Set.copyOf(states);
-    }
-    public Set<String> getAcceptingStates()
-    {
-        return Set.copyOf(acceptingStates);
-    }
-    public Set<Character> getAlphabet()
-    {
-        return Set.copyOf(alphabet);
-    }
-    public Map<String, Map<Character, String>> getTransitionMap()
-    {
-        return copyTransitionMap(transitionMap);
     }
 
     public boolean isAccepted(String input)
