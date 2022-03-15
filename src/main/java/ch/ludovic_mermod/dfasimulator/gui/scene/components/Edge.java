1a8c05e0618c9b2f4657074788fb0961ab09293b
Ludovic Mermod <ludovic.mermod@epfl.ch>
2022-03-15 12:09:33 +0100

    Fix positioning issues

--- src/main/java/ch/ludovic_mermod/dfasimulator/gui/scene/components/Edge.java
+++ src/main/java/ch/ludovic_mermod/dfasimulator/gui/scene/components/Edge.java
@@ -1,13 +1,12 @@
 package ch.ludovic_mermod.dfasimulator.gui.scene.components;
 
+import ch.ludovic_mermod.dfasimulator.Utils;
 import ch.ludovic_mermod.dfasimulator.gui.Constants;
 import ch.ludovic_mermod.dfasimulator.gui.scene.GraphPane;
 import ch.ludovic_mermod.dfasimulator.logic.State;
-import javafx.beans.value.ChangeListener;
-import javafx.collections.MapChangeListener;
+import javafx.beans.Observable;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
-import javafx.scene.control.MenuItem;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 
@@ -20,15 +19,11 @@ public class Edge extends Group
 
     private final Line line, leftLine, rightLine;
     private final Text alphabetDisplay;
-    private final GraphPane graphPane;
-
-    private MenuItem deleteMenuItem;
 
     public Edge(State source, State target, GraphPane graphPane)
     {
         this.source = source;
         this.target = target;
-        this.graphPane = graphPane;
 
         alphabetDisplay = new Text();
         updateAlphabetDisplay();
@@ -49,19 +44,7 @@ public class Edge extends Group
         rightLine.strokeWidthProperty().bind(Constants.Link.Line.width);
         rightLine.visibleProperty().bind(alphabetDisplay.textProperty().isEqualTo("").not());
 
-        updatePositions();
-
-        ChangeListener<Number> updatePos = (o, ov, nv) -> Edge.this.updatePositions();
-
-        source.getNode().layoutXProperty().addListener(updatePos);
-        source.getNode().layoutYProperty().addListener(updatePos);
-
-        target.getNode().layoutXProperty().addListener(updatePos);
-        target.getNode().layoutYProperty().addListener(updatePos);
-
-        Constants.Node.Circle.radius.addListener(updatePos);
-        Constants.Link.Text.distanceFromLine.addListener(updatePos);
-        Constants.Link.Line.sidelineLength.addListener(updatePos);
+        bindPositions();
 
         getChildren().addAll(line, leftLine, rightLine, alphabetDisplay);
     }
@@ -83,72 +66,6 @@ public class Edge extends Group
         return target.name();
     }
 
-    protected void bindSimulationPane(GraphPane graphPane)
-    {
-        deleteMenuItem.disableProperty().bind(graphPane.getSimulationProperty());
-    }
-
-    /*private ContextMenu createContextMenu()
-    {
-        menu = new ContextMenu();
-
-        deleteMenuItem = new MenuItem();
-        Strings.bind("delete", deleteMenuItem.textProperty());
-        deleteMenuItem.setOnAction(event -> graphPane.fi().deleteLink(link));
-        //delete.disableProperty().bind(getSimulatorParent().getSimulationProperty());
-
-        setOnMousePressed(event -> menu.hide());
-        setOnContextMenuRequested(event -> menu.show(this, event.getScreenX(), event.getScreenY()));
-
-        return menu;
-    }*/
-
-    private void updatePositions()
-    {
-        double radius = Constants.Node.Circle.radius.get();
-        Point2D startCenter = new Point2D(source.getNode().getLayoutX() + radius, source.getNode().getLayoutY() + radius),
-                endCenter = new Point2D(target.getNode().getLayoutX() + radius, target.getNode().getLayoutY() + radius),
-                director = endCenter.subtract(startCenter).normalize(),
-                normal = new Point2D(director.getY(), -director.getX()),
-                start = startCenter.add(director.multiply(Constants.Node.Circle.radius.get())),
-                end = endCenter.subtract(director.multiply(Constants.Node.Circle.radius.get()));
-
-        line.setStartX(start.getX());
-        line.setStartY(start.getY());
-        line.setEndX(end.getX());
-        line.setEndY(end.getY());
-
-        // Position the two lines from the side of the arrows
-        {
-            Point2D projectionPoint = end.subtract(director.multiply(Constants.Link.Line.sidelineLength.get())),
-                    projectionDistance = normal.multiply(Constants.Link.Line.sidelineLength.get()),
-                    leftStart = projectionPoint.add(projectionDistance),
-                    rightStart = projectionPoint.subtract(projectionDistance);
-
-            leftLine.setStartX(leftStart.getX());
-            leftLine.setStartY(leftStart.getY());
-            leftLine.setEndX(end.getX());
-            leftLine.setEndY(end.getY());
-
-            rightLine.setStartX(rightStart.getX());
-            rightLine.setStartY(rightStart.getY());
-            rightLine.setEndX(end.getX());
-            rightLine.setEndY(end.getY());
-        }
-
-        // Position the alphabet display
-        {
-            Point2D projectionRelative = start.add(end.subtract(start).multiply(Constants.Link.Text.distanceFromNodeFactor.get())),
-                    projectionAbsolute = start.add(director.multiply(Constants.Link.Text.distanceFromNodeAbsolute.get())),
-                    textPos = (Constants.Link.Text.usesAbsoluteDistance.get() && (projectionRelative.subtract(start).magnitude() > Constants.Link.Text.distanceFromNodeAbsolute.get()) ? projectionAbsolute : projectionRelative).add(normal.multiply(Constants.Link.Text.distanceFromLine.get()));
-
-            double angle = new Point2D(1, 0).angle(director);
-
-            alphabetDisplay.relocate(textPos.getX(), textPos.getY());
-            alphabetDisplay.setRotate(director.getY() > 0 ? angle : -angle);
-        }
-    }
-
     private void updateAlphabetDisplay()
     {
         alphabetDisplay.setText(source.transitionMap()
@@ -168,4 +85,64 @@ public class Edge extends Group
                 ", target=" + target.name() +
                 '}';
     }
+
+
+    private void bindPositions()
+    {
+        final Node sn = source.getNode();
+        final Node tn = target.getNode();
+
+        Observable[] observables = new Observable[]{
+                sn.layoutXProperty(), sn.layoutYProperty(), sn.widthProperty(), sn.heightProperty(),
+                tn.layoutXProperty(), tn.layoutYProperty(), tn.widthProperty(), tn.heightProperty(),
+                Constants.Node.Circle.radius,
+                Constants.Link.Line.sidelineLength,
+                Constants.Link.Text.distanceFromNodeFactor,
+                Constants.Link.Text.distanceFromNodeAbsolute,
+                Constants.Link.Text.usesAbsoluteDistance
+        };
+
+        Utils.bindDouble(line.startXProperty(), () -> computePoints().start.getX(), observables);
+        Utils.bindDouble(line.startYProperty(), () -> computePoints().start.getY(), observables);
+        Utils.bindDouble(line.endXProperty(), () -> computePoints().end.getX(), observables);
+        Utils.bindDouble(line.endYProperty(), () -> computePoints().end.getY(), observables);
+
+        Utils.bindDouble(leftLine.startXProperty(), () -> computePoints().leftLineStart.getX(), observables);
+        Utils.bindDouble(leftLine.startYProperty(), () -> computePoints().leftLineStart.getY(), observables);
+        Utils.bindDouble(leftLine.endXProperty(), () -> computePoints().end.getX(), observables);
+        Utils.bindDouble(leftLine.endYProperty(), () -> computePoints().end.getY(), observables);
+
+
+        Utils.bindDouble(rightLine.startXProperty(), () -> computePoints().rightLineStart.getX(), observables);
+        Utils.bindDouble(rightLine.startYProperty(), () -> computePoints().rightLineStart.getY(), observables);
+        Utils.bindDouble(rightLine.endXProperty(), () -> computePoints().end.getX(), observables);
+        Utils.bindDouble(rightLine.endYProperty(), () -> computePoints().end.getY(), observables);
+
+        Utils.bindDouble(alphabetDisplay.xProperty(), () -> computePoints().textPos.getX(), observables);
+        Utils.bindDouble(alphabetDisplay.yProperty(), () -> computePoints().textPos.getY(), observables);
+    }
+
+    private Points computePoints()
+    {
+        final Node sn = source.getNode();
+        final Node tn = target.getNode();
+
+        Point2D startCenter = new Point2D(sn.getLayoutX() + sn.getWidth() / 2, sn.getLayoutY() + sn.getHeight() / 2),
+                endCenter = new Point2D(tn.getLayoutX() + tn.getWidth() / 2, tn.getLayoutY() + tn.getHeight() / 2),
+                director = endCenter.subtract(startCenter).normalize(),
+                normal = new Point2D(director.getY(), -director.getX()),
+                start = startCenter.add(director.multiply(Constants.Node.Circle.radius.get())),
+                end = endCenter.subtract(director.multiply(Constants.Node.Circle.radius.get())),
+                projectionPoint = end.subtract(director.multiply(Constants.Link.Line.sidelineLength.get())),
+                projectionDistance = normal.multiply(Constants.Link.Line.sidelineLength.get()),
+                leftStart = projectionPoint.add(projectionDistance),
+                rightStart = projectionPoint.subtract(projectionDistance),
+                projectionRelative = start.add(end.subtract(start).multiply(Constants.Link.Text.distanceFromNodeFactor.get())),
+                projectionAbsolute = start.add(director.multiply(Constants.Link.Text.distanceFromNodeAbsolute.get())),
+                textPos = (Constants.Link.Text.usesAbsoluteDistance.get() && (projectionRelative.subtract(start).magnitude() > Constants.Link.Text.distanceFromNodeAbsolute.get()) ? projectionAbsolute : projectionRelative).add(normal.multiply(Constants.Link.Text.distanceFromLine.get()));
+
+        return new Points(start, end, leftStart, rightStart, textPos);
+    }
+
+    private record Points(Point2D start, Point2D end, Point2D leftLineStart, Point2D rightLineStart, Point2D textPos) {}
 }
