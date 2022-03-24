71b1bed27dcfc318a7ee24f6fe4696c46cd63214
Ludovic Mermod <ludovic.mermod@epfl.ch>
2022-03-24 07:13:13 +0100

    Fix issue and add methods
    Fix method Maths.circleIntersection
    Add methods Maths.approx, Maths.angle, Maths.angleCos, Maths.fromAngle, Maths.orthogonal, Maths.projection

--- src/main/java/ch/ludovic_mermod/dfasimulator/utils/Maths.java
+++ src/main/java/ch/ludovic_mermod/dfasimulator/utils/Maths.java
@@ -7,6 +7,7 @@ import java.util.function.Function;
 
 public class Maths
 {
+    private static final double EPSILON = 1e-7;
     public static double dichotomy(Function<Double, Double> f, double a, double b)
     {
         if (Math.signum(f.apply(a)) == Math.signum(f.apply(b)))
@@ -28,23 +29,75 @@ public class Maths
         return (neg + pos) / 2;
     }
 
-    public static Pair<Point2D, Point2D> circleIntersection(Point2D aCenter, double aRadius, Point2D bCenter, double bRadius)
+    public static Pair<Point2D, Point2D> circleIntersection(Point2D p0, double r0, Point2D p1, double r1)
     {
-        Point2D director = bCenter.subtract(aCenter).normalize(),
+        Point2D director = p1.subtract(p0).normalize(),
                 normal = new Point2D(-director.getY(), director.getX()).normalize();
 
-        if (aCenter.distance(bCenter) > (aRadius) + bRadius)
+        if (p0.distance(p1) > (r0) + r1)
             return new Pair<>(null, null);
-        else if (aCenter.distance(bCenter) == (aRadius) + bRadius)
-            return new Pair<>(aCenter.add(director.multiply(aRadius)), null);
+        else if (p0.distance(p1) == (r0) + r1)
+            return new Pair<>(p0.add(director.multiply(r0)), p0.add(director.multiply(r0)));
         else
         {
-            double d = aCenter.distance(bCenter),
-                    d1 = (aRadius + d - bRadius) / 2,
-                    alpha = Math.asin(d1 / aRadius),
-                    h = aRadius * Math.cos(alpha);
+            //https://stackoverflow.com/questions/3349125/circle-circle-intersection-points
+            double d = p0.distance(p1),
+                    x0 = p0.getX(), y0 = p0.getY(),
+                    x1 = p1.getX(), y1 = p1.getY();
 
-            return new Pair<>(aCenter.add(director.multiply(d1)).add(normal.multiply(h)), aCenter.add(director.multiply(d1)).subtract(normal.multiply(h)));
+            double a = (r0 * r0 - r1 * r1 + d * d) / (2 * d),
+                    h = Math.sqrt(r0 * r0 - a * a);
+
+            Point2D p2 = p0.add(p1.subtract(p0).normalize().multiply(a));
+            double x2 = p2.getX(), y2 = p2.getY();
+
+            return new Pair<>(
+                    new Point2D(
+                            x2 + h * (y1 - y0) / d,
+                            y2 - h * (x1 - x0) / d
+                    ),
+                    new Point2D(
+                            x2 - h * (y1 - y0) / d,
+                            y2 + h * (x1 - x0) / d
+                    )
+            );
         }
+    }
+
+    public static boolean approx(double a, double b)
+    {
+        return Math.abs(a - b) < EPSILON;
+    }
+    public static boolean approx(Point2D a, Point2D b)
+    {
+        return approx(a.getX(), b.getX()) && approx(a.getY(), b.getY());
+    }
+
+    public static double angle(Point2D p)
+    {
+        double a = Math.atan(p.getY() / p.getX());
+        if (p.getY() < 0) a += Math.PI;
+        return a;
+    }
+    public static double angleCos(Point2D a, Point2D b)
+    {
+        return a.dotProduct(b) / (a.magnitude() * b.magnitude());
+    }
+    public static double angle(Point2D a, Point2D b)
+    {
+        return Math.acos(angle(a, b));
+    }
+    public static Point2D fromAngle(double a)
+    {
+        return new Point2D(Math.cos(a), Math.sin(a));
+    }
+
+    public static Point2D orthogonal(Point2D p)
+    {
+        return new Point2D(-p.getY(), p.getX());
+    }
+    public static Point2D projection(Point2D point, Point2D target)
+    {
+        return target.normalize().multiply(point.magnitude() * angleCos(point, target));
     }
 }
