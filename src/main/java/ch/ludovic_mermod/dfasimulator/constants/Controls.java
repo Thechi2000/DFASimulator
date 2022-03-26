6cf357b9dbed283393a5472ebed89cc3b7de83ba
Ludovic Mermod <ludovic.mermod@epfl.ch>
2022-03-26 16:59:08 +0100

    Add shortcut for SettingsPane

--- src/main/java/ch/ludovic_mermod/dfasimulator/constants/Controls.java
+++ src/main/java/ch/ludovic_mermod/dfasimulator/constants/Controls.java
@@ -12,8 +12,9 @@ public class Controls
     public static final ObservableObjectValue<KeyCombination> dragTool = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.DIGIT2, KeyCombination.CONTROL_DOWN));
     public static final ObservableObjectValue<KeyCombination> linkTool = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.DIGIT3, KeyCombination.CONTROL_DOWN));
 
-    public static final ObservableObjectValue<KeyCombination> save    = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN));
-    public static final ObservableObjectValue<KeyCombination> saveAs  = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN, KeyCombination.SHIFT_DOWN));
-    public static final ObservableObjectValue<KeyCombination> open    = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
-    public static final ObservableObjectValue<KeyCombination> newFile = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_DOWN));
+    public static final ObservableObjectValue<KeyCombination> save     = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN));
+    public static final ObservableObjectValue<KeyCombination> saveAs   = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN, KeyCombination.SHIFT_DOWN));
+    public static final ObservableObjectValue<KeyCombination> open     = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
+    public static final ObservableObjectValue<KeyCombination> newFile  = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_DOWN));
+    public static final ObservableObjectValue<KeyCombination> settings = new SimpleObjectProperty<>(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN, KeyCombination.ALT_DOWN));
 }
