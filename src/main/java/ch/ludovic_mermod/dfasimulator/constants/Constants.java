e1f5bbeefc269a2cbc9f1cee3382d9e9e9916165
Ludovic Mermod <ludovic.mermod@epfl.ch>
2022-03-26 11:54:18 +0100

    Rework settings files format
    Settings are now saved in JSON files

--- src/main/java/ch/ludovic_mermod/dfasimulator/constants/Constants.java
+++ src/main/java/ch/ludovic_mermod/dfasimulator/constants/Constants.java
@@ -1,6 +1,9 @@
 package ch.ludovic_mermod.dfasimulator.constants;
 
 import ch.ludovic_mermod.dfasimulator.Main;
+import ch.ludovic_mermod.dfasimulator.json.JSONElement;
+import ch.ludovic_mermod.dfasimulator.json.JSONObject;
+import ch.ludovic_mermod.dfasimulator.logic.IOManager;
 import ch.ludovic_mermod.dfasimulator.utils.CustomBindings;
 import ch.ludovic_mermod.dfasimulator.utils.PropertiesMap;
 import javafx.beans.binding.Binding;
@@ -9,11 +12,8 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.Properties;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.regex.Pattern;
 
@@ -24,60 +24,85 @@ public class Constants
     public static final Font FONT = new Font(Font.getDefault().getName(), 20);
 
     private static final PropertiesMap<String, Object> values;
-    private static final Properties                    properties;
 
     private static final Pattern BOOL_PATTERN   = Pattern.compile("(true|false)");
     private static final Pattern DOUBLE_PATTERN = Pattern.compile("(\\d+\\.?\\d*|\\d*\\.\\d+)d?");
     private static final Pattern COLOR_PATTERN  = Pattern.compile("0x[\\da-fA-F]{8}");
 
+    private static JSONObject json;
+
     static
     {
         values = new PropertiesMap<>();
-        properties = new Properties();
         loadFromFile("settings.json");
-        values.addListener((p, k, o, n) -> properties.put(k, n.toString()));
+        //values.addListener((p, k, o, n) -> getSetting (k).);
+    }
+
+    public static JSONObject getSetting(String id)
+    {
+        List<String> path = Arrays.asList(id.split("\\."));
+        var setting = getSetting(json, path);
+
+        if (setting == null)
+            throw new IllegalArgumentException(String.format("Could not find setting \"%s\"", id));
+        else
+            return setting;
+    }
+    private static JSONObject getSetting(JSONObject object, List<String> path)
+    {
+        if (path.size() == 0)
+            return object;
+        else if (!object.hasObject(path.get(0)))
+            object.add(path.get(0), new JSONObject());
+
+        return getSetting(object.getAsJSONObject(path.get(0)), path.subList(1, path.size()));
     }
 
     public static void loadFromFile(String filename)
     {
-        try (FileReader fileReader = new FileReader(filename))
+        var readJSON = JSONElement.readFromFile(filename);
+        json = readJSON.isJSONObject() ? readJSON.getAsJSONObject() : new JSONObject();
+        values.clear();
+
+        try
         {
-            properties.load(fileReader);
+            addEntries(json, "");
         }
-        catch (IOException e)
+        catch (IOManager.CorruptedFileException e)
         {
-            Main.logger.log(Level.WARNING, "Could not load settings from file \"%s\"", filename);
+            Main.logger.log(Level.SEVERE, "Could not parse settings file", e);
         }
-
-        properties.keySet().stream().map(Object::toString).forEach(k -> {
-            String value = properties.getProperty(k);
-            ObjectProperty<Object> obs = null;
-
-            if (value.matches(BOOL_PATTERN.pattern()))
-                obs = new SimpleObjectProperty<>(Boolean.valueOf(value));
-            else if (value.matches(COLOR_PATTERN.pattern()))
-                obs = new SimpleObjectProperty<>(Color.valueOf(value));
-            else if (value.matches(DOUBLE_PATTERN.pattern()))
-                obs = new SimpleObjectProperty<>(Double.parseDouble(value));
-            else
-                Main.log(Level.WARNING, "Could not parse property \"%s:%s\" in \"%s\"", k, value, filename);
-
-            if (obs != null)
-            {
-                obs.addListener((o, ov, nv) -> properties.put(k, nv.toString()));
-                values.put(k, obs);
-            }
-        });
     }
     public static void saveToFile(String filename)
     {
-        try (FileWriter fileWriter = new FileWriter(filename))
+        json.saveToFile(filename);
+    }
+
+    private static void addEntries(JSONObject object, String objectPath) throws IOManager.CorruptedFileException
+    {
+        if (object.has(".value") && object.has(".type"))
         {
-            properties.store(fileWriter, LocalDateTime.now().toString());
+            String type = object.get(".type").getAsString();
+            JSONElement valueElement = object.get(".value");
+            ObjectProperty<Object> obs = switch (type)
+                    {
+                        case "double" -> new SimpleObjectProperty<>(valueElement.getAsDouble());
+                        case "boolean" -> new SimpleObjectProperty<>(valueElement.getAsBoolean());
+                        case "color" -> new SimpleObjectProperty<>(Color.valueOf(valueElement.getAsString()));
+                        default -> throw new IOManager.CorruptedFileException("Unknown type \"%s\" at \"%s\"", valueElement.getAsString(), object);
+                    };
+
+            object.addProperty(".value", obs);
+            values.put(objectPath, obs);
         }
-        catch (IOException e)
+        else
         {
-            e.printStackTrace();
+            if (!objectPath.isBlank()) objectPath = objectPath + ".";
+            for (var e : object.entrySet())
+            {
+                if (!e.getValue().isJSONObject()) throw new IOManager.CorruptedFileException("Could not convert \"%s\" to JSONObject", objectPath + e.getKey());
+                addEntries(e.getValue().getAsJSONObject(), objectPath + e.getKey());
+            }
         }
     }
 
@@ -93,13 +118,24 @@ public class Constants
     {
         return get(key, BLACK);
     }
-
     public static <T> Binding<T> get(String key, T defaultValue)
     {
         if (!values.containsKey(key))
         {
             Main.log(Level.WARNING, "Could not find property \"%s\" of type %s", key, defaultValue.getClass().getName());
-            values.setValue(key, defaultValue);
+
+            SimpleObjectProperty<Object> prop = new SimpleObjectProperty<>(defaultValue);
+            values.put(key, prop);
+
+            var sett = getSetting(key);
+            sett.addProperty(".value", prop);
+            sett.addProperty(".type", switch (defaultValue.getClass().getSimpleName())
+                    {
+                        case "Double" -> "double";
+                        case "Boolean" -> "boolean";
+                        case "Color" -> "color";
+                        default -> "string";
+                    });
         }
 
         var value = values.get(key);
@@ -119,21 +155,8 @@ public class Constants
     {
         return getValue(key, BLACK);
     }
-
     public static <T> T getValue(String key, T defaultValue)
     {
-        if (!values.containsKey(key))
-        {
-            Main.log(Level.WARNING, "Could not find property \"%s\" of type %s", key, defaultValue.getClass().getName());
-            values.setValue(key, defaultValue);
-        }
-
-        var value = values.get(key);
-
-        if (defaultValue instanceof Double && value.get() instanceof Double)
-            return (T) value.get();
-
-        if (defaultValue.getClass().isAssignableFrom(value.get().getClass())) return (T) value.get();
-        else throw new IllegalArgumentException(String.format("Property \"%s\" is not of type \"%s\"", defaultValue.getClass().getName(), value.get().getClass().getName()));
+        return get(key, defaultValue).getValue();
     }
 }
